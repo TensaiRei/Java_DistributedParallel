255376b9070efeeb143aac0494a4f6872cc52afa
Rei <snowy.sunny.dazing.us@gmail.com>
2024-03-18 23:07:12 +0900

    - Update

---
+++ src/main/java/_06_Networking/session00_on_class/client/Client.java
@@ -0,0 +1,21 @@
+package _06_Networking.session00_on_class.client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+
+		try (Socket socket = new Socket("192.168.1.124", 7878);) {
+			System.out.println("Clent side: Client connected at port 7878");
+			while (socket.isConnected()) {
+				System.out.println("Client side: Client is still connected");
+				Thread.sleep(1000);
+			}
+		}
+
+	}
+
+}
---
+++ src/main/java/_06_Networking/session00_on_class/server/Server.java
@@ -0,0 +1,28 @@
+package _06_Networking.session00_on_class.server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) {
+
+		ServerSocket serverSocket = null;
+
+		try {
+			serverSocket = new ServerSocket(7878);
+			System.out.println("Server side: Server is running on port 7878");
+
+			while (true) {
+				Socket socket = serverSocket.accept();
+				System.out.println("Server side: A client connected" 
+				+ " "  + socket.getInetAddress() 
+				+ ":" + socket.getPort());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
