d0ee494fa60ce2035148a948af85d23db94d9c52
Rei <snowy.sunny.dazing.us@gmail.com>
2024-03-23 16:43:25 +0900

    - Update

--- src/main/java/_04_Neo4j/session01_northwind/dao/DAO_Customer.java
+++ src/main/java/_04_Neo4j/session01_northwind/dao/DAO_Customer.java
@@ -1,15 +1,10 @@
 package _04_Neo4j.session01_northwind.dao;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.neo4j.driver.Driver;
-import org.neo4j.driver.Result;
-import org.neo4j.driver.Session;
 
 import _04_Neo4j.session01_northwind.connect.ConnectionNeo4j;
 import _04_Neo4j.session01_northwind.entity.Customer;
-import _04_Neo4j.session01_northwind.utillity.JsonUtility;
 
 public class DAO_Customer {
 
@@ -24,20 +19,7 @@ public class DAO_Customer {
 	}
 
 	public List<Customer> getListOfCustomers() {
-		// MATCH (c:Customer) RETURN c
-		String query = "MATCH (c:Customer) RETURN c";
-		try (Session session = driver.session()) {
-			return session.executeRead(transaction -> {
-				Result result = transaction.run(query);
-				return result.stream()
-						.collect(Collectors.toList())
-						.stream()
-						.map(record -> record.get("c").asNode())
-						.map(node -> JsonUtility.convertNodeToPOJO(node, Customer.class))
-						.collect(Collectors.toList());
-
-			});
-		}
+		return null;
 	}
 
 }
