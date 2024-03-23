fb5a2626baa1346c899e415827fb63abcb4c02b5
Rei <snowy.sunny.dazing.us@gmail.com>
2024-03-23 17:38:15 +0900

    - Update

---
+++ src/test/java/_05_JPA_ORM/session01/JTC_Connection.java
@@ -0,0 +1,45 @@
+package _05_JPA_ORM.session01;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class JTC_Connection {
+	
+	private static EntityManagerFactory factory;
+	private static EntityManager manager;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		factory = Persistence.createEntityManagerFactory("JPA_ORM_01 MSSQL");
+		manager = factory.createEntityManager();
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		manager.close();
+		factory.close();
+		System.out.println("Connection closed!");
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void connection() {
+		String query = "SELECT 'Connection successful!'";
+		System.out.println(manager.createNativeQuery(query).getSingleResult());
+	}
+
+}
